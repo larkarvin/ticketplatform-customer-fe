6e4848e0efa053eb33bdf079c103d777f58faffc
larkarvin <arvin@almar.io>
2026-07-12 23:14:00 +0800

    Add submission edit page with locked priced fields

---
+++ app/pages/submissions/[slug]/edit.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SubmissionEditor from '~/features/forms/components/SubmissionEditor.vue'
+import { useSubmissionEdit } from '~/features/forms/composables/useSubmissionEdit'
+
+definePageMeta({ layout: 'form' })
+
+const route = useRoute()
+const state = await useSubmissionEdit(String(route.params.slug))
+</script>
+
+<template>
+  <SubmissionEditor :state="state" />
+</template>
